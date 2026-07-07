20244d3a1c6f3f838950fe56086db911d158ff45
CarlosSoles <carlos13soles@gmail.com>
2026-07-07 00:20:55 -0500

    Fix logout page routing for Streamlit Cloud

--- src/views/logout.py
+++ src/views/logout.py
@@ -4,6 +4,6 @@ def logout_page():
     st.session_state.logged_in = False
     st.session_state.role = None
     st.session_state.user_info = None
-    st.switch_page("src/views/auth.py")
+    st.rerun()
 
 logout_page()
